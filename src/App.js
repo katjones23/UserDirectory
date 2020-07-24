3ed274fc0e8b60ba93f821cf5cd215fd5bb7ba3b
Katherine Jones <kejone1414@gmail.com>
2020-07-24 17:51:07 -0400

    feat: minor edit to index.html, removed app.css, modified app.js to reflect removal of css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 
 function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>
           Hello world
         </p>
